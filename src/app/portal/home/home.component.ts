0411ea7be0bbc3b9de15f05a90d87a4c60ac4cfb
Vinicius <vinicius.01deagosto@gmail.com>
2022-07-01 09:40:12 -0300

    feature: criação da seção de estatística

--- src/app/portal/home/home.component.ts
+++ src/app/portal/home/home.component.ts
@@ -1,23 +1,24 @@
 import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 
 import { PresentationComponent } from './presentation/presentation.component';
+import { StatsComponent } from './stats/stats.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.sass'],
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('presentation', { read: ViewContainerRef })
+  entryPresentation?: ViewContainerRef;
+  @ViewChild('stats', { read: ViewContainerRef }) entryStats?: ViewContainerRef;
 
-  @ViewChild('presentation', { read: ViewContainerRef }) entryPresentation?: ViewContainerRef
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
-   this.entryPresentation?.createComponent(PresentationComponent)
+    this.entryPresentation?.createComponent(PresentationComponent);
+    this.entryStats?.createComponent(StatsComponent);
   }
 }
