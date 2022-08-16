1a90d595fc9301759a4c7ce4c170f8f40d545225
Vinicius <vinicius.01deagosto@gmail.com>
2022-08-16 17:46:33 -0300

    feature: criando seção para envio de messagem

---
+++ src/app/portal/home/subscription-newsletter/subscription-newsletter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subscription-newsletter',
+  templateUrl: './subscription-newsletter.component.html',
+  styleUrls: ['./subscription-newsletter.component.sass']
+})
+export class SubscriptionNewsletterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
